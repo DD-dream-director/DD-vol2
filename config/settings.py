ffd9ecab192b71028081697d5a3623ad6a4d7978
Yoshiyuki Kurose <moyashikuro1225@gmail.com>
2024-02-02 23:11:12 +0900

    add : accountsアプリが追加されたことをconfig/settings.pyに追記

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',  # accountsアプリをインストール
     'dd_app',  # dd_appをインストール
     'taggit',  # taggitをインストール
     'django_extensions',  # django-extensionsをインストール
