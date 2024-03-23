a5fca99224b2af702ff943bde62e46ad87d40f68
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-03-23 13:30:30 +0900

    upgrade : DBにサムネイルを保存するためのカラムを追加

--- dd_app/models.py
+++ dd_app/models.py
@@ -7,16 +7,18 @@ from taggit.managers import TaggableManager
 
 
 class Video(models.Model):
-    """動画を管理するためのORM
-    """
+    """動画を管理するためのORM"""
+
     # 動画をアップロードしたユーザーを管理するためのフィールド
     uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
     # 動画のタイトルを管理するためのフィールド
     title = models.CharField(max_length=100)
+    # 動画のサムネイルをアップロードするためのフィールド
+    thumbnail = models.ImageField(upload_to="thumbnails/", blank=True, null=False)
     # 動画の説明を管理するためのフィールド
     description = models.TextField()
     # 動画ファイルをアップロードするためのフィールド
-    video_file = models.FileField(upload_to='videos/', blank=True, null=True)
+    video_file = models.FileField(upload_to="videos/", blank=True, null=True)
     # 動画URLを管理するためのフィールド
     video_url = models.URLField(blank=True, null=True)
     # いつ動画がアップロードされたかを管理するためのフィールド
@@ -30,25 +32,23 @@ class Video(models.Model):
 
     class Meta:
         # テーブル名をvideoに設定
-        db_table = 'video'
+        db_table = "video"
 
     def __str__(self):
         # 動画のタイトルを返す(管理画面で表示されるカラムの設定)
         return self.title
 
     def clean(self):
-        """動画ファイルと動画URLのどちらか一方が必要なことを確認するためのメソッド
-        """
+        """動画ファイルと動画URLのどちらか一方が必要なことを確認するためのメソッド"""
         if self.video_file is None and self.video_url is None:
             raise ValidationError("ファイルとURLのうちどちらか一つは値が必要です")
 
 
 class Comment(models.Model):
-    """動画に対するコメントを管理するためのORM
-    """
+    """動画に対するコメントを管理するためのORM"""
+
     # コメントがどの動画に対するものかを管理するためのフィールド
-    video = models.ForeignKey(
-        Video, on_delete=models.CASCADE, related_name='comments')
+    video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name="comments")
     # コメントの投稿者を管理するためのフィールド
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     # コメントの本文を管理するためのフィールド
@@ -58,7 +58,7 @@ class Comment(models.Model):
 
     class Meta:
         # テーブル名をcommentに設定
-        db_table = 'comment'
+        db_table = "comment"
 
     def __str__(self):
         # コメントの本文を返す(管理画面で表示されるカラムの設定)
