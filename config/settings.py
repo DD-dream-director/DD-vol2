cf87c37096412993c83acd13e6d38e03ccd7fbc6
taimat64 <c1219371@st.kanazawa-it.ac.jp>
2024-04-11 18:15:48 +0900

    2024_4_11

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 動画ファイルのルート
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media') 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
+
+
--- dd_app/views.py
+++ dd_app/views.py
@@ -5,11 +5,15 @@ import re
 from django.views.generic import View ,DetailView
 from .models import Video
 import re
+from moviepy.editor import VideoFileClip
+from django.core.files.storage import default_storage
+
 
 
 class Rendering_View(View):
     def get(self, request, *args, **kwargs):
         video_ids = []
+        video_files = []
         video_titles =[]
         video_pks = []
         videos = Video.objects.all()
@@ -17,15 +21,18 @@ class Rendering_View(View):
         for video in videos:
             video_titles.append(video.title)
             video_pks.append(video.pk)
-            video_id_match = re.search(r'(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})', video.video_url)
 
-            if video_id_match:
-                video_ids.append(video_id_match.group(1))
-            
+            if video.video_file:
+                # video_file = MOV_Conversion(video.video_file)
+                
+                video_ids.append(video.video_file)
+
+            else:
+                video_id_match = re.search(r'(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})', video.video_url)
+                video_ids.append(video_id_match.group(1)) 
             
         ziplist = zip(video_ids, video_titles,  video_pks)
                 
-
         return render(request, 'youtube_embed.html', {'ziplist': ziplist})
 
 
@@ -33,7 +40,22 @@ class Rendering_View(View):
 class DetailVideoView(DetailView):
     template_name = 'detail.html'
     model = Video
-    context_object_name = "video"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        video = self.object
+        youtube_video_id = self.get_youtube_video_id(video.video_url)
+        context['youtube_video_id'] = youtube_video_id
+        return context
+
+    def get_youtube_video_id(self, url):
+        video_id_match = re.search(r'(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})', url)
+        if video_id_match:
+            return video_id_match.group(1)
+        return None
+
+
+
 
     
 
