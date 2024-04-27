091480b0913b58dbda31d3a7519f602ac756490b
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-04-27 09:53:38 +0900

    add : 動画投稿の技術検証は完了。

--- dd_app/urls.py
+++ dd_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from dd_app.views import VideoCreateView
+from dd_app.views import VideoCreateView, TopPageView
 
 urlpatterns = [
     path("video_upload/", VideoCreateView.as_view(), name="video_uploading_test"),
+    path("", TopPageView.as_view(), name="home"),
 ]
--- dd_app/views.py
+++ dd_app/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from .models import Video
@@ -14,7 +15,7 @@ class VideoCreateView(CreateView):
     model = Video
     form_class = VideoForm
     template_name = "video_uploading_test.html"
-    # success_url = reverse_lazy("video_list")  # 成功時のリダイレクト先
+    success_url = reverse_lazy("home")  # 成功時のリダイレクト先
 
     def form_valid(self, form):
         """フォームのバリデーション処理を書く場所"""
@@ -23,3 +24,15 @@ class VideoCreateView(CreateView):
             admin_user  # ログインユーザーをadminユーザーに指定(テスト用)
         )
         return super().form_valid(form)
+
+
+class TopPageView(TemplateView):
+    """トップページを表示するビュー"""
+
+    template_name = "video_uploading_RedirectDestination_test.html"
+
+    def get_context_data(self, **kwargs):
+        """テンプレートに渡す変数を追加する"""
+        context = super().get_context_data(**kwargs)
+        context["video_list"] = Video.objects.all().order_by("-uploaded_at")[:5]
+        return context
