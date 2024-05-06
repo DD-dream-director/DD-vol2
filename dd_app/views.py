24400ac0609a57576e3425d99df496dfd31f98ee
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-05-06 19:39:56 +0900

    delete : 技術検証用のスクリプトを削除

--- dd_app/views.py
+++ dd_app/views.py
@@ -4,33 +4,3 @@ from django.shortcuts import render
 import re
 from django.views.generic import DetailView
 from .models import Video
-
-#URLを埋め込みコードに変換
-def get_youtube_video_id(url):
-    video_id_match = re.search(r'(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})', url)
-    if video_id_match:
-        return video_id_match.group(1)
-    return None
-
-#フロントに送信
-#URLがあるときとないときをif文で
-def youtube_embed(request):
-    youtube_urls = Video.objects.all()
-    for youtube_url in youtube_urls:
-        video_id = get_youtube_video_id(youtube_url.video_url)
-        
-    return render(request, 'ren_app/youtube_embed.html', {'video_id': video_id})
-
-#詳細画面
-class DetaiVideoView(DetailView):
-    template_name = 'ren_app/detail.html'
-    model = Video
-
-    def get_object(self, queryset=None):
-        # ここで適切なオブジェクトを取得するロジックを実装します。
-        return Video.objects.get(pk=self.kwargs['pk'])
-
-
-    
-
-    
