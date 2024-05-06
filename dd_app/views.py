570038ac5755fe028c5d0d1df573eca6f1550ddb
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-05-06 19:58:45 +0900

    add : シンプルなテンプレートクラスビューを追加した

--- dd_app/views.py
+++ dd_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+
+class LoginView(TemplateView):
+    template_name = "login.html"
+
+
+class RegisterUserView(TemplateView):
+    template_name = "register_user.html"
+
+
+class PostMovieView(TemplateView):
+    template_name = "post_movie.html"
+
+
+class SetupTagView(TemplateView):
+    template_name = "setup_tag.html"
+
+
+class ShowVideoDetailView(TemplateView):
+    template_name = "show_video_detail.html"
