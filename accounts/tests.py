f4648c5605d0f4593f371239845efd55a82a6ba9
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-05-12 21:33:48 +0900

    add : login機能のバックエンドのテストコードを追加

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,11 +1,13 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
 
 
 class SignUpViewTests(TestCase):
+    """アカウント登録バックエンドのテストコード"""
 
     def test_signup_page_status_code(self):
         response = self.client.get(reverse("signup"))
@@ -27,3 +29,36 @@ class SignUpViewTests(TestCase):
         )
         self.assertEqual(response.status_code, 302)
         self.assertTrue(User.objects.filter(username="testuser").exists())
+
+
+class LoginViewTests(TestCase):
+    """ログインバックエンドのテストコード"""
+
+    def setUp(self):
+        self.username = "testuser"
+        self.password = "Testpass123"
+        self.user = get_user_model().objects.create_user(
+            username=self.username, password=self.password
+        )
+
+    def test_login_page_status_code(self):
+        response = self.client.get(reverse("login"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page_template(self):
+        response = self.client.get(reverse("login"))
+        self.assertTemplateUsed(response, "login.html")
+
+    def test_login_form_valid(self):
+        response = self.client.post(
+            reverse("login"), {"username": self.username, "password": self.password}
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("home"))
+
+    def test_login_form_invalid(self):
+        response = self.client.post(
+            reverse("login"), {"username": self.username, "password": "wrongpassword"}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.context["user"].is_authenticated)
