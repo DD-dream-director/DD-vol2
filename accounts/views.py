fe73063994110cdb00fb8192847aa8b5c395be1a
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-05-12 20:07:51 +0900

    add : アカウントのバックエンドを作成

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse
+
 
 # Create your views here.
+class CustomLoginView(LoginView):
+    template_name = "login.html"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse("home")  # ログイン成功後のリダイレクト先
--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("dd_app/", include("dd_app.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
