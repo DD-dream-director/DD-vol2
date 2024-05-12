30b0345dd19087e659e52a6eaab78e8915740f90
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-05-12 21:58:13 +0900

    add : dd_app forms.py

--- dd_app/views.py
+++ dd_app/views.py
@@ -2,6 +2,13 @@ from django.db.models.base import Model as Model
 from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.views import View
+from django.utils.decorators import method_decorator
+from .forms import VideoForm
+from .models import Video
+
 
 # Create your views here.
 
