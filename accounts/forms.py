f1432086688c897a4193063aa8b7355d3b93ebba
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-05-23 19:38:23 +0900

    update : アカウント作成フォーム用のDjangoHTMLTemplateを適用

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,9 +3,51 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
+# class SignUpForm(UserCreationForm):
+#     email = forms.EmailField(required=True)
+
+#     class Meta:
+#         model = User
+#         fields = ("username", "email", "password1", "password2")
+
+
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    username = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "ユーザー名を入力"}
+        ),
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(
+            attrs={"class": "form-control", "placeholder": "メールアドレスを入力"}
+        ),
+    )
+    password1 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "パスワードを入力"}
+        ),
+    )
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "パスワードの再確認"}
+        ),
+    )
 
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+        widgets = {
+            "username": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "ユーザー名を入力"}
+            ),
+            "password1": forms.PasswordInput(
+                attrs={"class": "form-control", "placeholder": "パスワードを入力"}
+            ),
+            "password2": forms.PasswordInput(
+                attrs={"class": "form-control", "placeholder": "パスワードの再確認"}
+            ),
+        }
