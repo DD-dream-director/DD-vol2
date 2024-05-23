9382f5f93f32f204e8dd8409ca81527c6fd409ce
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-05-23 19:40:09 +0900

    style : 不要なコメントを削除

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,14 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-# class SignUpForm(UserCreationForm):
-#     email = forms.EmailField(required=True)
-
-#     class Meta:
-#         model = User
-#         fields = ("username", "email", "password1", "password2")
-
-
 class SignUpForm(UserCreationForm):
     username = forms.CharField(
         required=True,
